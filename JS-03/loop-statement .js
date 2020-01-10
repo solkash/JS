b50750a0f9fb5618769808acd4026e3eef761d33
Solomiia Shymanska <51424180+solkash@users.noreply.github.com>
2020-01-10 18:23:25 +0200

    for..in and for..of

--- JS-03/loop-statement .js
+++ JS-03/loop-statement .js
@@ -1,6 +1,6 @@
 function giveMeFive(obj) {
   const five = [];
-  for (let key in obj) {
+  for (const key in obj) {
     if (key.length === 5) five.push(key);
     if (obj[key].length === 5) five.push(obj[key]);
   }
